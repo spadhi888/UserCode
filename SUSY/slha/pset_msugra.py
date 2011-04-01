b745da7c4b34e4b41b9d8bf395d22fc43a0bb9ef
spadhi <>
2011-04-01 10:24:21 +0000

    slha files and psets

---
+++ SUSY/slha/pset_msugra.py
@@ -0,0 +1,151 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.222.2.6 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: LM1_sfts_cfi.py -s GEN,FASTSIM,HLT:GRun --pileup=NoPileUp --geometry DB -n 10 --conditions START38_V12::All --datatier GEN-SIM-DIGI-RECO --eventcontent RECOSIM --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLT')
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('FastSimulation.Configuration.RandomServiceInitialization_cff')
+process.load('FastSimulation.PileUpProducer.PileUpSimulator7TeV_cfi')
+process.load('FastSimulation.Configuration.FamosSequences_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('FastSimulation.Configuration.FamosSequences_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedParameters_cfi')
+process.load('FastSimulation.Configuration.HLT_GRun_cff')
+process.load('FastSimulation.Configuration.CommonInputs_cff')
+process.load('FastSimulation.Configuration.EventContent_cff')
+
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string(': 1.222.2.6 $'),
+    annotation = cms.untracked.string('LM1_sfts_cfi.py nevts:10'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10000)
+)
+
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('OtherCMS', 
+        'StdException', 
+        'Unknown', 
+        'BadAlloc', 
+        'BadExceptionType', 
+        'ProductNotFound', 
+        'DictionaryNotFound', 
+        'InsertFailure', 
+        'Configuration', 
+        'LogicError', 
+        'UnimplementedFeature', 
+        'InvalidReference', 
+        'NullPointerError', 
+        'NoProductSpecified', 
+        'EventTimeout', 
+        'EventCorruption', 
+        'ScheduleExecutionFailure', 
+        'EventProcessorFailure', 
+        'FileInPathError', 
+        'FileOpenError', 
+        'FileReadError', 
+        'FatalRootError', 
+        'MismatchedInputFiles', 
+        'ProductDoesNotSupportViews', 
+        'ProductDoesNotSupportPtr', 
+        'NotFound')
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+# Output definition
+
+process.RECOSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    outputCommands = process.RECOSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('reco.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-DIGI-RECO')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.famosPileUp.PileUpSimulator = process.PileUpSimulatorBlock.PileUpSimulator
+process.famosPileUp.PileUpSimulator.averageNumber = 0
+process.famosSimHits.SimulateCalorimetry = True
+process.famosSimHits.SimulateTracking = True
+process.simulation = cms.Sequence(process.simulationWithFamos)
+process.HLTEndSequence = cms.Sequence(process.reconstructionWithFamos)
+
+# set correct vertex smearing
+process.Realistic7TeVCollisionVtxSmearingParameters.type = cms.string("BetaFunc")
+process.famosSimHits.VertexGenerator = process.Realistic7TeVCollisionVtxSmearingParameters
+process.famosPileUp.VertexGenerator = process.Realistic7TeVCollisionVtxSmearingParameters
+# Apply ECAL/HCAL miscalibration
+process.ecalRecHit.doMiscalib = True
+process.hbhereco.doMiscalib = True
+process.horeco.doMiscalib = True
+process.hfreco.doMiscalib = True
+# Apply Tracker and Muon misalignment
+process.famosSimHits.ApplyAlignment = True
+process.misalignedTrackerGeometry.applyAlignment = True
+
+process.misalignedDTGeometry.applyAlignment = True
+process.misalignedCSCGeometry.applyAlignment = True
+
+process.GlobalTag.globaltag = 'START38_V12::All'
+
+process.generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring(
+            'MSEL=0         ! User defined processes', 
+            'MSEL=39                  ! All SUSY processes ', 
+            'IMSS(1) = 11             ! Spectrum from external SLHA file', 
+            'IMSS(21) = 33            ! LUN number for SLHA File (must be 33) ', 
+            'IMSS(22) = 33            ! Read-in SLHA decay table '),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters', 
+            'SLHAParameters'),
+        SLHAParameters = cms.vstring("SLHAFILE = NotDefined'           ! Name of the SLHA spectrum file")
+    )
+)
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+process.RandomNumberGeneratorService.generator.initialSeed = 2088
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(cms.SequencePlaceholder("randomEngineStateProducer"))
+process.reconstruction = cms.Path(process.reconstructionWithFamos)
+process.RECOSIMoutput_step = cms.EndPath(process.RECOSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.reconstruction,process.RECOSIMoutput_step])
+
+# special treatment in case of production filter sequence
+for path in process.paths: 
+    getattr(process,path)._seq = process.ProductionFilterSequence*getattr(process,path)._seq
+
+
